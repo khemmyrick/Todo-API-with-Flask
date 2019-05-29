bde19bb509b920c639b5b3822a7b95d53c24d9a2
Khem Myrick <khem.myrick@gmail.com>
2019-05-28 20:02:46 -0700

    Initial upload.

---
+++ models.py
@@ -0,0 +1,23 @@
+import datetime
+
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
+                          BadSignature, SignatureExpired)
+from peewee import *
+
+import config
+
+DATABASE = SqliteDatabase('todos.sqlite')
+SERIALIZER = Serializer(config.SECRET_KEY)
+
+class Todo(Model):
+    name = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect(reuse_if_open=True)
+    DATABASE.create_tables([Todo], safe=True)
+    DATABASE.close()
---
+++ resources/todos.py
@@ -0,0 +1,97 @@
+from flask import jsonify, Blueprint, abort
+
+from flask_restful import (Resource, Api, reqparse,
+                           inputs, fields, marshal,
+                           marshal_with, url_for)
+# https://teamtreehouse.com/community/no-module-named-flaskext
+
+import models
+
+todo_fields = {
+    'name': fields.String
+}
+# flask restful's list and url fields don't play nice together
+
+
+def todo_or_404(todo_id):
+    try:
+        todo = models.Todo.get(models.Todo.id==todo_id)
+    except models.Todo.DoesNotExist:
+        abort(404)
+    else:
+        return todo
+
+
+class TodoList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            required=True,
+            help='No todo text provided.',
+            location=['form', 'json']
+        )
+        super().__init__()
+
+    def get(self):
+        todos = [marshal(todo, todo_fields)
+                   for todo in models.Todo.select()]
+        return todos
+        # todos = models.Todo.select()
+        # todos = Todo.query.all()
+        # return {'todos': todos}
+        # return models.SERIALIZER.dumps({'todos': todos})
+
+    @marshal_with(todo_fields)
+    def post(self):
+        args = self.reqparse.parse_args()
+        todo = models.Todo.create(**args)
+        return (
+            todo,
+            201, {'Location': url_for('resources.todos.todo', id=todo.id)}
+        )
+
+
+class Todo(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            required=True,
+            help='No todo text provided',
+            location=['form', 'json']
+        )
+        super().__init__()
+
+    @marshal_with(todo_fields)
+    def get(self, id):
+        return todo_or_404(id)
+
+    @marshal_with(todo_fields)
+    def put(self, id):
+        args = self.reqparse.parse_args()
+        query = models.Todo.update(**args).where(models.Todo.id==id)
+        query.execute()
+        return (
+            models.Todo.get(models.Todo.id==id),
+            200, {'Location': url_for('resources.todos.todo', id=id)}
+        )
+
+    def delete(self, id):
+        query = models.Todo.delete().where(models.Todo.id==id)
+        query.execute()
+        return '', 204, {'Location': url_for('resources.todos.todos')}
+
+
+todos_api = Blueprint('resources.todos', __name__)
+api = Api(todos_api)
+api.add_resource(
+    TodoList,
+    '/todos',
+    endpoint='todos'
+)
+api.add_resource(
+    Todo,
+    '/todos/<int:id>',
+    endpoint='todo'
+)
